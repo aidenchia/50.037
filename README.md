4d75b3134797886f884e4d9eebe6b3a849662780
aidenchia <aidenchia95@gmail.com>
2020-04-11 23:14:16 +0800

    question 1a

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # 50.037
 Blockchain Technology Module Exercises
+
+## Question 1
+__Connect Web3py to Ganache__
+
+```
+ganache_url = "http://127.0.0.1:7545
+web3 = Web3(Web3.HTTPProvider(ganache_url))
+```
+
+__Make transactions between Ganache accounts__
---
+++ app.py
@@ -0,0 +1,32 @@
+from web3 import Web3 
+
+ganache_url = "http://127.0.0.1:7545"
+web3 = Web3(Web3.HTTPProvider(ganache_url))
+
+account_1 = '0xdb7BD110BeDadbb3DF35dd0907C0369E4412DBBD' 
+account_2 = '0xe0D49AA4Bab0b0af92F0F16c72BF3d610Ddb1F5f'
+private_key = '39f86dabeed31f945362790582d87641ae35b309a44ffbf998161ed083806c4c'
+
+nonce = web3.eth.getTransactionCount(account_1)
+
+tx = {
+	'nonce': nonce,
+	'to': account_2,
+	'value': web3.toWei(1, 'ether'),
+	'gas': 2000000,
+	'gasPrice': web3.toWei('50', 'gwei')
+}
+
+# sign transaction
+signed_tx = web3.eth.account.signTransaction(tx, private_key)
+
+# send transaction to the network
+tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+
+# print to stdout the hash of the transaction
+print(web3.toHex(tx_hash))
+
+
+
+
+
